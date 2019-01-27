9535deb742042010d8d35984663d42a9df3e50c8
Kim HyunWoo <stories2@naver.com>
2019-01-27 12:54:09 +0900

    Setup: Load android opencv-lib successfully

--- app/src/main/java/com/project/smartmirror/MainActivity.java
+++ app/src/main/java/com/project/smartmirror/MainActivity.java
@@ -2,12 +2,38 @@ package com.project.smartmirror;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends Activity {
+
+    static String TAG = "opencv";
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onCreate :: Internal OpenCV library not found.");
+        } else {
+            Log.d(TAG, "onCreate :: OpenCV library found inside package. Using it!");
+        }
     }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 }
